5342e54a63846de10e15c248b7449820fe02d6de
tomas <tomascostapp@gmail.com>
2023-09-26 22:27:34 -0300

    linkeo css personales

--- wp-content/themes/customecommerce/header.php
+++ wp-content/themes/customecommerce/header.php
@@ -21,6 +21,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/customecommerce.css'?>">
 	<link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/colchoneriavip.css'?>">
+	<link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/juan.css'?>">
+	<link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/luka.css'?>">
 </head>
 
 <body <?php body_class(); ?>>
